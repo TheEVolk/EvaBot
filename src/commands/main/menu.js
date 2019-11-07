613fd45026c77ccbbd2c64d7a0f5aa6baf1484df
TheEVolk <elektro-volk@yandex.ru>
2019-11-07 11:43:13 +0300

    Новое меню

--- src/commands/main/menu.js
+++ src/commands/main/menu.js
@@ -1,49 +1,46 @@
-import { Keyboard } from 'vk-io'
+import { Keyboard } from 'vk-io';
 
 class StSubcommand {
   name = 'статья'
 
-  handler (ctx) {
+  handler(ctx) {
     ctx.builder()
       .text('⬜ Приятного прочтения.\nvk.com/@bot_eva-who')
       .keyboard(Keyboard.builder()
-        .textButton({ label: `Назад`, payload: { command: 'меню' } })
-        .oneTime()
-      )
-      .answer()
+        .textButton({ label: 'Назад', payload: { command: 'меню' } })
+        .oneTime())
+      .answer();
   }
 }
 
 class SupportSubcommand {
   name = 'поддержка'
 
-  handler (ctx) {
+  handler(ctx) {
     ctx.builder()
       .text('⬜ Тех. поддержка: vk.me/evabottp')
       .keyboard(Keyboard.builder()
-        .textButton({ label: `Назад`, payload: { command: 'меню' } })
-        .oneTime()
-      )
-      .answer()
+        .textButton({ label: 'Назад', payload: { command: 'меню' } })
+        .oneTime())
+      .answer();
   }
 }
 
 class OtherSubcommand {
   name = 'прочее'
 
-  handler (ctx) {
+  handler(ctx) {
     ctx.builder()
       .text('Прочее:')
       .keyboard(Keyboard.builder()
-        .textButton({ label: `💬 Чат`, payload: { command: 'беседа' } })
-        .textButton({ label: `📋 О боте`, payload: { command: 'меню статья' } })
+        .textButton({ label: '💬 Чат', payload: { command: 'беседа' } })
+        .textButton({ label: '📋 О боте', payload: { command: 'меню статья' } })
         .row()
-        .textButton({ label: `Поддержка`, payload: { command: 'меню поддержка' } })
-        .textButton({ label: `Донат`, color: 'primary', payload: { command: 'донат' } })
+        .textButton({ label: 'Поддержка', payload: { command: 'меню поддержка' } })
+        .textButton({ label: 'Донат', color: 'primary', payload: { command: 'донат' } })
         .inline(ctx.clientInfo.inline_keyboard === true)
-        .oneTime()
-      )
-      .answer({ mainMenu: false })
+        .oneTime())
+      .answer({ mainMenu: false });
   }
 }
 
@@ -57,28 +54,23 @@ export default class MenuCommand {
     new OtherSubcommand()
   ]
 
-  async handler (ctx) {
+  async handler(ctx) {
     ctx.builder()
       .lines([
         `Баланс: ${ctx.user.money.toLocaleString('ru')} бит.`
       ])
       .keyboard(Keyboard.builder()
-        .textButton({ label: `Профиль`, payload: { command: 'профиль' } })
-        .textButton({ label: `Ачивки`, payload: { command: 'ачивки' } })
+        .textButton({ label: 'Профиль', payload: { command: 'профиль' } })
+        .textButton({ label: 'Ачивки', payload: { command: 'ачивки' } })
         .row()
-        .textButton({ label: `Работа`, payload: { command: 'работа' } })
-        // .row()
-        // .textButton({ label: `Донат`, payload: { command: 'донат' } })
-        .textButton({ label: `Прочее`, payload: { command: 'меню прочее' } })
+        .textButton({ label: 'Работа', payload: { command: 'работа' } })
+        .textButton({ label: 'Банк', payload: { command: 'банк' } })
         .row()
-        .textButton({ label: '🆕 Кейсы', color: 'primary', payload: { command: 'кейс' } })
+        .textButton({ label: '🆕', color: 'primary', payload: { command: 'рассылка' } })
+        .textButton({ label: 'Ⓜ️', payload: { command: 'меню прочее' } })
+        .textButton({ label: '💵', payload: { command: 'донат' } })
         .inline(ctx.clientInfo.inline_keyboard === true)
-        .oneTime()
-        // .textButton({ label: `${petEmoji} Питомец`, payload: { command: 'пит' } })
-        // .textButton({ label: `${jobEmoji} Работа`, payload: { command: 'работа' } })
-        // .row()
-        // .textButton({ label: '🏙 Город', payload: { command: 'город' } })
-      )
-      .answer({ mainMenu: false })
+        .oneTime())
+      .answer({ mainMenu: false });
   }
 }
